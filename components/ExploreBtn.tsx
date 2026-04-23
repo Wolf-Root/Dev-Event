bb4458450e9e8c89f8a3f414e9199c94e099be7f
Wolf-Root <mobilemax363@gmail.com>
2026-04-23 21:12:53 +0100

    Fix the width

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ExploreBtn() {
   return (
     <a href="#evants">
-      <button type="button" id="explore-btn" className="mt-7 mx-auto">
+      <button type="button" id="explore-btn" className="w-fit mt-7 mx-auto">
         Explore Events
         <Image src="/icons/arrow-down.svg" alt="Arrow Down" width={24} height={24} />
       </button>
